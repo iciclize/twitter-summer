0af9347b57f993b9723b5b72346da1f53c4cc4b9
Ubuntu <ice@ysfh.qbq3jmefzhjepdftw054xok1ra.lx.internal.cloudapp.net>
2016-08-04 10:51:06 +0900

    {PERCENT}に対応

--- left.js
+++ left.js
@@ -23,12 +23,17 @@
 
 
 /*
+* DATE_OF_SUMMER_VACATION_START    夏休み初日の日付(Dateオブジェクト)
 * DATE_OF_SCHOOL_START    学校が始まる日付(Dateオブジェクト)を入れて差し上げろ
-* TEMPLATE    名前のテンプレート。{HOURS}は残り時間数に、{DAYS}は残り日数に置換される
+* TEMPLATE    名前のテンプレート。以下のキーワードはそれに対応した数値に置換される
+*     - {HOURS}    残り時間数
+*     - {DAYS}     残り日数
+*     - {PERCENT}  残り時間の百分率
 */
 
+var DATE_OF_SUMMER_VACATION_START = new Date('2016-7-23 UTC+0900');
 var DATE_OF_SCHOOL_START = new Date('2016-8-29 UTC+0900');
-var TEMPLATE = 'いの #夏休み残り{HOURS}時間';
+var TEMPLATE = 'いの #夏休み残量{PERCENT}%';
 
 
 
@@ -43,7 +48,8 @@ var now = new Date();
 var timespan_ms = DATE_OF_SCHOOL_START.getTime() - now.getTime();
 var lefts = {
     days: Math.floor(timespan_ms / A_DAY_MS),
-    hours: Math.floor(timespan_ms / AN_HOUR_MS)
+    hours: Math.floor(timespan_ms / AN_HOUR_MS),
+    percent: Math.round( 100 * Math.timespan_ms / (DATE_OF_SCHOOL_START.getTime() - DATE_OF_SUMMER_VACATION_START.getTime()) ) / 100;
 };
 
 
@@ -51,6 +57,7 @@ var profile = {
     name: TEMPLATE
              .replace('{DAYS}', lefts.days)
              .replace('{HOURS}', lefts.hours)
+             .replace('{PERCENT}', left.percent)
 };
 
 // プロフィールの変更を実行
